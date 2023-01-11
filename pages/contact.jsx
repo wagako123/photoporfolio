5beb76225b3229b0e2cc85f4544b5c7a4a87d5cd
unknown <brianwagako@gmail.com>
2023-01-11 14:30:55 +0300

    contactpage

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Contact from '../components/Contact'
+import Hero from '../components/Hero'
 
 const contact = () => {
   return (
-    <div>contact</div>
+    <div>
+        <Hero heading="Let's work together" message='Kindly fill the form for quotes and booking' />
+        <Contact />
+    </div>
   )
 }
 
